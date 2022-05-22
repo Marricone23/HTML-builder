40a4d8131c55bff4d96e82a1dacacf32f99cef20
Marricone23 <marricone23@gmail.com>
2022-05-22 23:22:54 +0300

    feat: update 04-copy-files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+let fs = require('fs');
+const path = require('path');
+const dirPath = 'C:\\Users\\marri\\Desktop\\HTML-builder\\04-copy-directory\\files\\';
 
+fs.mkdir('04-copy-directory/files-copy', err => {
+   if(err) throw err; // не удалось создать папку
+   console.log('Папка успешно создана');
+});
+
+fs.readdir(dirPath, (err, files) => {
+   if(err) throw err; // не прочитать содержимое папки
+   console.log('В папке находятся файлы:' + files);
+});
+
+
+//let { COPYFILE_EXCL } = fs.constants;
+fs.copyFile('files', 'files-copy', err => {
+   if(err) throw err; // не удалось скопировать файл. Он уже существует?
+   console.log('Файл успешно скопирован');
+});

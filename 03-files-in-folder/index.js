3d7d2e613fa393667195180013b15fa9ebe9615a
Marricone23 <marricone23@gmail.com>
2022-05-23 16:47:45 +0300

    update 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -13,28 +13,11 @@ const path = require('path')
 
 const dirPath = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\03-files-in-folder\\secret-folder\\';
 
-const filess = fs.readdirSync(dirPath).map(fileName => `${dirPath}\\${fileName}`);
-console.log(filess.join('\n'));
-
-const files = fs.readdirSync(dirPath, {withFileTypes: true})
+/*const files = fs.readdirSync(dirPath, {withFileTypes: true})
 .filter(item => !item.isDirectory())
 .map(item => item.name)
 
 console.log(files.join('\n'));
-
-/*fs.readdir( dirPath, {withFileTypes: true}, (err, files) => {
-  files.forEach((item)=>{
-     let itemPath = path.join(item);
-     console.log(itemPath)
-  })
-  if(err) throw err; // не прочитать содержимое папки
-  console.log('В папке находятся файлы:' + files);
-
-});*/
-
-/*const stat = fs.statSync(dirPath);
-console.log(stat.isDirectory());
-console.log("Path is file:", stat.isFile());*/
 
 files.forEach(file => {fs.stat(dirPath, (error, stats) => {
     if (error) {
@@ -46,4 +29,28 @@ files.forEach(file => {fs.stat(dirPath, (error, stats) => {
       console.log("Path is directory:", stats.isDirectory());
     }
   });
-})
+})*/
+
+
+fs.readdir( dirPath, (err, files) => {
+  files.forEach((item)=>{
+     let itemPath = path.join(item);
+     console.log(itemPath)
+     const extname = path.extname(item)
+     console.log(extname)
+     fs.stat(dirPath, (error, stats) => {   
+       if (error) {
+         console.log(error);
+       }
+       else {
+         console.log(stats);
+         console.log("Path is file:", stats.isFile());
+         console.log("Path is directory:", stats.isDirectory());
+       }   
+});
+   
+  })
+  if(err) throw err; // не прочитать содержимое папки
+  console.log('В папке находятся файлы:' + files);
+
+});

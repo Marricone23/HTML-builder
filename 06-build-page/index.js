697fe0705dab389d90f4ba8804cbfce775d9e2c0
Marricone23 <marricone23@gmail.com>
2022-05-24 14:49:06 +0300

    update folder assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,6 +4,10 @@ const dirPathAssets = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-p
 const dirPathComponents = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\components\\';
 const dirPathStyles = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\styles\\';
 const dirPathNew = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\project-dist\\';
+const to = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\project-dist\\assets\\img';
+const dirPathAssetsImg = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\assets\\img';
+const dirPathAssetsSvg = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\assets\\svg';
+const toDirPathAssetsSvg = 'C:\\Users\\a.asouski\\Desktop\\HTML-builder-1\\06-build-page\\project-dist\\assets\\svg';
 
 fs.mkdir('06-build-page/project-dist', { recursive: true }, (err) => {
    if (err) throw err;// папка уже существует
@@ -39,21 +43,73 @@ fs.writeFile(dirPathNew + "index.html", "", function(error){
     }; 
 });
 
-
 fs.copyFile(dirPathAssets +'/fonts/' + 'Karolina-Regular.woff2', '06-build-page/project-dist/assets/fonts/Karolina-Regular.woff2',err => {
     if(err) throw err; // не удалось скопировать файл
     console.log('Файл успешно скопирован');
- });/*
- fs.copyFile(dirPath + 'test-image.jpg', '04-copy-directory/files-copy/test-image-copy.jpg',err => {
-    if(err) throw err; // не удалось скопировать файл
-    console.log('Файл успешно скопирован');
  });
- fs.copyFile(dirPath + 'test-js.js', '04-copy-directory/files-copy/test-js-copy.js',err => {
-    if(err) throw err; // не удалось скопировать файл
-    console.log('Файл успешно скопирован');
- });
- fs.copyFile(dirPath + 'test-text.txt', '04-copy-directory/files-copy/test-text-copy.txt',err => {
-    if(err) throw err; // не удалось скопировать файл
-    console.log('Файл успешно скопирован');
- });*/
+
+
+/*function copyFolderSync(dirPathAssetsImg, to ) {
+    fs.readdirSync(dirPathAssetsImg).forEach(element => {
+        if (fs.lstatSync(path.join(dirPathAssetsImg, element)).isFile()) {
+            fs.copyFileSync(path.join(dirPathAssetsImg, element), path.join(to, element));
+        } else {
+            copyFolderSync(path.join(dirPathAssetsImg, element), path.join(to, element));
+        }
+    });
+}
+copyFolderSync(dirPathAssetsImg, to);*/
+
+function copyFolderSync(dirPathAssetsImg, to ) {
+    fs.readdir(dirPathAssetsImg,( err,files ) => 
+    files.forEach(element => {
+        if (fs.lstatSync(path.join(dirPathAssetsImg, element)).isFile()) {
+            fs.copyFile(path.join(dirPathAssetsImg, element), path.join(to, element), err => {
+                if(err) throw err; // не удалось скопировать файл
+                console.log('Файл успешно скопирован');
+            });
+        } else {
+            copyFolder(path.join(dirPathAssetsImg, element), path.join(to, element), err => {
+                if(err) throw err; // не удалось скопировать файл
+                console.log('Файл успешно скопирован');
+            });
+        }
+    })
+   
+);  
+}
+copyFolderSync(dirPathAssetsImg, to);
+
+/*function copyFolderSvg(dirPathAssetsSvg, toDirPathAssetsSvg ) {
+    fs.readdirSync(dirPathAssetsSvg).forEach(element => {
+        if (fs.lstatSync(path.join(dirPathAssetsSvg, element)).isFile()) {
+            fs.copyFileSync(path.join(dirPathAssetsSvg, element), path.join(toDirPathAssetsSvg, element));
+        } else {
+            copyFolderSync(path.join(dirPathAssetsSvg, element), path.join(toDirPathAssetsSvg, element));
+        }
+    });
+}
+copyFolderSvg(dirPathAssetsSvg, toDirPathAssetsSvg);*/
+
+
+function copyFolderSvg(dirPathAssetsSvg, toDirPathAssetsSvg ) {
+    fs.readdir(dirPathAssetsSvg,( err,files ) => 
+    files.forEach(element => {
+        if (fs.lstatSync(path.join(dirPathAssetsSvg, element)).isFile()) {
+            fs.copyFile(path.join(dirPathAssetsSvg, element), path.join(toDirPathAssetsSvg, element), err => {
+                if(err) throw err; // не удалось скопировать файл
+                console.log('Файл успешно скопирован');
+            });
+        } else {
+            copyFolder(path.join(dirPathAssetsSvg, element), path.join(toDirPathAssetsSvg, element), err => {
+                if(err) throw err; // не удалось скопировать файл
+                console.log('Файл успешно скопирован');
+            });
+        }
+    })
+   
+);  
+}
+copyFolderSvg(dirPathAssetsSvg, toDirPathAssetsSvg);
+
  
